7b6ed0f99357ced2d0f1321f3f757b4ecdef7b01
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-25 22:02:30 +0900

    trying to find ROUTER error

--- front/src/UI/organisms/Navigation/index.tsx
+++ front/src/UI/organisms/Navigation/index.tsx
@@ -1,39 +1,14 @@
 import React from 'react';
-import { Grid } from '@chakra-ui/react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import { NavIcons } from '../../molecules/NavIcons';
 import { NavigationData } from './NavigationData';
-
-import Main from '../../../pages/Main';
-import Profile from '../../../pages/Profile';
-import Chat from '../../../pages/Chat';
-import Game from '../../../pages/Game';
-
 import './index.scss';
 
 export const Navigation = () => {
   return (
-    <Grid templateRows="repeat(5, 1fr)" gap={6}>
-      <Router>
-        <div className="navigation-container">
-          <NavIcons NavigationData={NavigationData}></NavIcons>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/chat">
-            <Chat />
-          </Route>
-          <Route path="/game">
-            <Game />
-          </Route>
-        </Switch>
-      </Router>
-    </Grid>
+    <>
+      <div className="navigation-container">
+        <NavIcons NavigationData={NavigationData}></NavIcons>
+      </div>
+    </>
   );
 };
--- front/src/templates/Layout/index.tsx
+++ front/src/templates/Layout/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Flex, Box, Center } from '@chakra-ui/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navigation } from '../../UI/organisms/Navigation';
+import Main from '../../pages/Main';
+import Profile from '../../pages/Profile';
+import Chat from '../../pages/Chat';
+import Game from '../../pages/Game';
 
 const Layout = () => {
   return (
     <>
       <Flex minH="100vh" flexDirection="row">
-        <Box width="85px"></Box>
+        <Box width="85px">
+          <Navigation></Navigation>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Main />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path="/chat">
+                <Chat />
+              </Route>
+              <Route path="/game">
+                <Game />
+              </Route>
+            </Switch>
+          </Router>
+        </Box>
         <Center width="full"></Center>
         <Box width="390px" bg="green" />
       </Flex>
