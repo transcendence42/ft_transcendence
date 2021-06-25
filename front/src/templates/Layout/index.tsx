cbca5e4f3333e41c22b604a9e5257a442e1bbc7c
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-25 16:36:13 +0900

    chore: update Chakra modules

---
+++ front/src/templates/Layout/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
+
+const Layout = () => {
+  return (
+    <>
+      <Grid h="1024" templateColumns="repeat(288, 1fr)">
+        <GridItem w="85" colSpan={17} bg="papayawhip" />
+        <GridItem colSpan={193} bg="tomato" />
+        <GridItem colSpan={78} bg="papayawhip" />
+      </Grid>
+    </>
+  );
+};
+
+export default Layout;
