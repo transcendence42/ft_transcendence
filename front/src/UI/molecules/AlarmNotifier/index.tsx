1d7485a1e1a660da55b7583956c55655713b91e2
hochan Lee <hochan049@gmail.com>
2021-06-28 22:00:56 +0900

    alarm message 구현 완료

--- front/src/UI/molecules/AlarmNotifier/index.tsx
+++ front/src/UI/molecules/AlarmNotifier/index.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 
-import { AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Box, Text } from '@chakra-ui/react';
+import { AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Box, Text, Flex } from '@chakra-ui/react';
+
+import { AlarmMessage } from '../../atoms/AlarmMessage';
+
+const messageData = [
+  {
+    id: '123412a',
+    title: '친구요청',
+    content: '42_Dall 님이 친구 요청을 보냈습니다.',
+    alarmTime: '1시간전',
+  },
+  {
+    id: '3242323a',
+    title: '게임패배',
+    content: 'jwon 님과 경기에서 패배했습니다.',
+    alarmTime: '1시간전',
+  },
+  {
+    id: '12334a',
+    title: '게임승리',
+    content: 'Polarbear 님과 경기에서 승리했습니다.',
+    alarmTime: '1시간전',
+  },
+];
+
+export interface IalarmMessage {
+  title: string;
+  content: string;
+  alarmTime: string;
+}
 
 export const AlarmNotifier = () => {
   return (
@@ -15,10 +44,12 @@ export const AlarmNotifier = () => {
           <AccordionIcon />
         </AccordionButton>
       </h2>
-      <AccordionPanel pb={4}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat.
+      <AccordionPanel p={1} bg="gray.50">
+        <Flex flexDirection="column">
+          {messageData.map(({ id, title, content, alarmTime }) => (
+            <AlarmMessage key={id} title={title} content={content} alarmTime={alarmTime} />
+          ))}
+        </Flex>
       </AccordionPanel>
     </AccordionItem>
   );
