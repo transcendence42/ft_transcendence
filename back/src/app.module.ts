16b5b93f16bc279a82902f80c755f9788d243443
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-28 12:47:28 +0900

    chore: GraphQL 설치 - yarn add @nestjs/graphql graphql-tools graphql apollo-server-express

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
