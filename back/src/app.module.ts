d4de0a1a768cc0c1e49adbc70d24266348182b20
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-28 17:04:23 +0900

    chore: postgresql, typeorm - yarn add pg typeorm @nestjs/typeorm class-validator class-transformer

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
