8bb6d4c47268eeb2937942d35fc4c6aaa9309133
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-28 18:15:33 +0900

    feat: sample CRUD data - GraphQL, TypeORM 실행을 위한 sample data 추가 - 실행시 postgresql에 table이 자동 생성되며, postgresql이 구성되지 않은 상태에서 실행시 에러 발생이 예측 - sample data로 user를 생성하고 이에 대한 CRUD를 GraphQL로 테스트함

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot(),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
