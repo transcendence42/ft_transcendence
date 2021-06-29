817fdf5b3d9b7188955d0183af07bce300757d8b
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-29 13:57:34 +0900

    refactor: template/Main to template/MiddleSection & MainHeader to MiddleSectionHeader

--- front/src/pages/Main/index.tsx
+++ front/src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainPageTemplate from '../../templates/MiddleSection';
+import MiddleSection from '../../templates/MiddleSection';
 import ProfileLarge from '../../UI/organisms/MiddleSectionHeader';
 import MainButtons from '../../UI/molecules/MainButtons';
 import GameCard from '../../UI/organisms/GameCard';
@@ -7,9 +7,9 @@ import { DummyGameCardData } from '../../UI/organisms/GameCard/DummyGameCardData
 
 const Main: React.FC = () => {
   return (
-    <MainPageTemplate mainHeader={<ProfileLarge rightComponent={<MainButtons />} />}>
+    <MiddleSection mainHeader={<ProfileLarge rightComponent={<MainButtons />} />}>
       <GameCard playerA={DummyGameCardData.playerA} playerB={DummyGameCardData.playerB} />
-    </MainPageTemplate>
+    </MiddleSection>
   );
 };
 
