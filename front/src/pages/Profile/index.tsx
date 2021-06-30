68c97b0857c9db673729437aae5ea8da1b33f1f2
jwon42 <jwon@student.42seoul.kr>
2021-06-30 13:26:38 +0900

    feat: add profile pages

--- front/src/pages/Profile/index.tsx
+++ front/src/pages/Profile/index.tsx
@@ -1,53 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MiddleSectionTemplate from '../../templates/MiddleSection';
-import { Table, Thead, Tr, Td, Th, Tbody, Tfoot } from '@chakra-ui/react';
+import ProfileLarge from '../../UI/molecules/ProfileLarge';
+// import { ProfileSmall } from '../../UI/molecules';
+import { Table, Thead, Tbody, Tr, Td, Th, Box } from '@chakra-ui/react';
+
+const TableHeader = () => {
+  const headerMeta = ['#', '점수', '게임시간', '게임일시', '상대방프로필'];
+
+  return (
+    <>
+      <Thead>
+        <Tr>
+          {headerMeta.map((d, i) => (
+            <Th key={i}>{d}</Th>
+          ))}
+        </Tr>
+      </Thead>
+    </>
+  );
+};
+
+const TableRow = ({ key, data }) => {
+  const [RowData] = useState(data);
+
+  return (
+    <>
+      <Tr key={key}>
+        <Td>{RowData.index}</Td>
+        <Td>{RowData.score}</Td>
+        <Td>{RowData.playTime}</Td>
+        <Td>{RowData.createdAt}</Td>
+        <Td>{RowData.adversary}</Td>
+      </Tr>
+    </>
+  );
+};
+
+const testData = () => {
+  return {
+    index: '1',
+    score: '3:2',
+    playTime: '4분 42초',
+    createdAt: '2021-06-21 15:35',
+    adversary: 'jwon',
+  };
+};
+
+const ProfileTable = (props) => {
+  const [tableData] = useState([testData()]);
+
+  return (
+    <>
+      <Box bg="white">
+        {tableData.length !== 0 && (
+          <Table>
+            <TableHeader />
+            <Tbody>
+              {tableData.map((d, i) => {
+                return <TableRow key={i} data={d} />;
+              })}
+            </Tbody>
+          </Table>
+        )}
+      </Box>
+    </>
+  );
+};
 
 const Profile: React.FC = () => {
   return (
-    <MiddleSectionTemplate>
-      <Table variant="simple" size="lg" cellPadding="10px">
-        <Thead>
-          <Tr borderTop="3px" borderColor="blue.400">
-            <Th>#</Th>
-            <Th>점수</Th>
-            <Th>게임시간</Th>
-            <Th isNumeric>일시</Th>
-            <Th isNumeric>상대방 프로필</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>1</Td>
-            <Td>승</Td>
-            <Td>4분 42초</Td>
-            <Td isNumeric>2021-06-21 15:35</Td>
-            <Td isNumeric>profile small (right align)</Td>
-          </Tr>
-          <Tr>
-            <Td>2</Td>
-            <Td>패</Td>
-            <Td>4분 42초</Td>
-            <Td isNumeric>2021-06-21 15:35</Td>
-            <Td isNumeric>profile small (right align)</Td>
-          </Tr>
-          <Tr>
-            <Td>3</Td>
-            <Td>승</Td>
-            <Td>4분 42초</Td>
-            <Td isNumeric>2021-06-21 15:35</Td>
-            <Td isNumeric>profile small (right align)</Td>
-          </Tr>
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>#</Th>
-            <Th>점수</Th>
-            <Th>게임시간</Th>
-            <Th isNumeric>일시</Th>
-            <Th isNumeric>상대방 프로필</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
+    <MiddleSectionTemplate middleSectionHeader={<ProfileLarge />}>
+      <ProfileTable />
     </MiddleSectionTemplate>
   );
 };
