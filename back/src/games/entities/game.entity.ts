5637a46c996ac3482f906d1265d8015227151bcc
yechoi42 <yechoi@student.42seoul.kr>
2021-07-01 11:04:40 +0900

    feat: game entity

--- back/src/games/entities/game.entity.ts
+++ back/src/games/entities/game.entity.ts
@@ -1,7 +1,42 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@ObjectType()
+
+@Entity('game') // typeORM decorator
+@ObjectType() // graphQL decorator
 export class Game {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: false }) // typeORM decorator
+  @Field(() => Boolean, { defaultValue: false }) // graphQL decorator
+  isPlaying: boolean;
+
+  @Column({ length: 15 })
+  @Field(() => String, { nullable: true })
+  winnerId: string;
+
+  @Column()
+  @Field(() => Int)
+  winnerScore: number;
+
+  @Column({ length: 15 })
+  @Field(() => String, { nullable: true })
+  loserId: string;
+
+  @Column()
+  @Field(() => Int)
+  loserScore: number;
+
+  @Column()
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Column()
+  @Field(() => Date)
+  finishedAt: Date;
+
+  @Column()
+  @Field(() => Date)
+  modifiedAt: Date;
 }
