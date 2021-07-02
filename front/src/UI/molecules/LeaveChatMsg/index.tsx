603d3a633be254df43bd959ce2c2df41a9921e99
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-07-02 19:29:34 +0900

    refactor: leaveChatMsg - css class 변경

--- front/src/UI/molecules/LeaveChatMsg/index.tsx
+++ front/src/UI/molecules/LeaveChatMsg/index.tsx
@@ -7,7 +7,7 @@ export const LeaveChatMsg = () => {
     <>
       <Text>회원님이 생성한 채팅방입니다.</Text>
       <Text>
-        <span className="leave-chat-msg">대화내용 및 채팅방 정보가 모두 삭제</span>
+        <span className="leave-chat-warning-msg">대화내용 및 채팅방 정보가 모두 삭제</span>
         됩니다.
       </Text>
     </>
