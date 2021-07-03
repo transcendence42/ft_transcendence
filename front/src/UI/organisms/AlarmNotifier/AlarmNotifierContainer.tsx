5c71fbafdd3b109b3d8acdd6d09169e42e2f01cd
Hochan Lee <holee@c7r7s8.42seoul.kr>
2021-07-03 13:52:00 +0900

    feat: query pollInterval 기능 추가

--- front/src/UI/organisms/AlarmNotifier/AlarmNotifierContainer.tsx
+++ front/src/UI/organisms/AlarmNotifier/AlarmNotifierContainer.tsx
@@ -5,7 +5,9 @@ import { AlarmNotifierPresenter } from './AlarmNotifierPresenter';
 import { GET_ALARM_NOTIFIER, REMOVE_ALARM_NOTIFIER } from './AlarmNotifierQueries';
 
 export const AlarmNotifierContainer = () => {
-  const { loading, error, data } = useQuery(GET_ALARM_NOTIFIER);
+  const { loading, error, data } = useQuery(GET_ALARM_NOTIFIER, {
+    pollInterval: 2000,
+  });
   const [removeAlarmMessage] = useMutation(REMOVE_ALARM_NOTIFIER);
 
   const removeAlarmMessageHandler = (alarmIndex: number) => {
