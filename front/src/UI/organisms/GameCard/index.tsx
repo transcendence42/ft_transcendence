e8c1e3aaf66206acbb780718773ad03e52375eb0
yechoi42 <yechoi@student.42seoul.kr>
2021-07-05 16:57:47 +0900

    feat: runningTime

--- front/src/UI/organisms/GameCard/index.tsx
+++ front/src/UI/organisms/GameCard/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Box, Text, Button, Icon } from '@chakra-ui/react';
 import { MdTimer } from 'react-icons/md';
 import ProfileMedium from '../../molecules/ProfileMedium';
 import { GameCardProps } from '../../../utils/interface';
 import './index.scss';
 
-const getRunningTime = (startTime: Date): string => {
+const getRunningTime = (isoStartTime: Date): string => {
   const currentTime: Date = new Date();
-  const isoStartTime: Date = new Date(startTime);
   const rawGameTime = currentTime.getTime() - isoStartTime.getTime();
   const min = String(Math.floor(rawGameTime / 1000 / 60));
   const sec = String(Math.floor(rawGameTime / 1000) - Number(min) * 60);
@@ -15,13 +14,25 @@ const getRunningTime = (startTime: Date): string => {
 };
 
 const GameCard = ({ playerA, playerB, startTime }: GameCardProps) => {
+  const isoStartTime: Date = useMemo(() => new Date(startTime), [startTime]);
+  const [runningTime, setRunningTime] = useState(getRunningTime(isoStartTime));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRunningTime(getRunningTime(isoStartTime));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isoStartTime]);
+
   return (
     <>
       <Box className="game-card">
         <Box className="game-card-running-time-section">
           <div>
             <Icon as={MdTimer} />
-            {getRunningTime(startTime)}
+            {runningTime}
           </div>
         </Box>
         <Box className="game-card-profile-section">
