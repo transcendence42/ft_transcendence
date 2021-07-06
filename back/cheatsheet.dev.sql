b2026bca9724537709d1cb8311561105d58f057e
hochan Lee <hochan049@gmail.com>
2021-07-06 14:10:10 +0900

    feat: add sql cheatsheet (#90)
    * feat: add sql cheatsheet
    
    * Update back/cheatsheet.sql
    
    * feat: drop, select 추가
    
    * feat: add game data
    
    * eol
    
    * feat: add user sql dummy data
    
    * feat: chat, chat_log dummy data
    - chat, chat_log 더미 데이터 추가
    
    * feat: add user sql dummy data(ladderRating, tatalWin, tatalLose, friendID, blockID)
    
    * feat: modify dummy data
    
    * feat: add user dummy data(avatar, userState)
    
    * Update back/cheatsheet.dev.sql
    
    Co-authored-by: Hochan Lee <holee@c7r7s8.42seoul.kr>
    Co-authored-by: yechoi42 <yechoi@student.42seoul.kr>
    Co-authored-by: jwon42 <jwon@student.42seoul.kr>
    Co-authored-by: Younghak Shin <yshin@c2r10s8.42seoul.kr>

---
+++ back/cheatsheet.dev.sql
@@ -0,0 +1,50 @@
+\du
+\dt
+
+SELECT * FROM alarm;
+SELECT * FROM game;
+SELECT * FROM user;
+
+DROP TABLE alarm;
+DROP TABLE game;
+DROP TABLE user;
+
+INSERT INTO alarm ("userID", title, content, checked, "type", link) VALUES
+('holee', '친구요청', '42_Dall님이 친구 요청을 보냈습니다.', false, 'addFriend', '/profile'),
+('holee', '게임패배', 'jwon님과 경기에서 패배했습니다.', false, 'defeat', '/profile'),
+('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile'),
+('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile');
+
+INSERT INTO game ("playerOneID", "playerOneScore", "playerTwoID", "playerTwoScore") VALUES
+('holee', 0, 'yshin', 1),
+('yechoi', 3, 'yshin', 7),
+('holee', 2, 'jwon', 5),
+('yshin', 6, 'jwon', 4);
+
+INSERT INTO public.user ("userID", nickname, "ladderRating", "totalWin", "totalLose", "friendID", "blockID", avatar, "userState") VALUES
+('jwon', 'junho won', 800, 5, 6, '{yechoi, holee}', '{devil}', '', 'logout'),
+('yechoi', 'yeji choi', 1200, 6, 5, '{jwon, yshin}', '{devil}', '', 'login'),
+('holee', 'hochan lee', 1600, 7, 4, '{jwon, yechoi, yshin}', '{devil}', '', 'playing'),
+('yshin', 'younghak shin', 1800, 8, 4, '{jwon, yechoi}', '{devil}', 'https://cdn.intra.42.fr/users/medium_yshin.jpg', 'playing'),
+('devil', 'who i am', -2400, 0, 12, '{}', '{}', '', 'login');
+
+INSERT INTO chat (name, password, "isAlive", type, "ownerID", "adminID", "userID")
+  VALUES
+    ('채팅방1', '', true, 'public', 'yshin', '{}', '{holee, yshin, devil}'),
+    ('채팅방2', '', false, 'public', 'yshin', '{}', '{yshin}'),
+    ('채팅방3', '', true, 'public', 'yshin', '{jwon, yechoi}', '{holee, jwon, yechoi, yshin, devil}'),
+    ('채팅방4', '1234', true, 'private', 'holee', '{}', '{holee, jwon, yechoi, yshin}'),
+    ('채팅방5', '1234', true, 'private', 'jwon', '{holee, yechoi}', '{holee, jwon, yechoi}'),
+    ('채팅방6', '', true, 'dm', 'holee', '{}', '{holee, jwon}'),
+    ('채팅방7', '', true, 'dm', 'yshin', '{}', '{holee, yshin}')
+;
+
+--uuid 컬럼은 uuid 형식을 따르며, chat 테이블의 uuid 컬럼을 참조하므로 서브 쿼리문을 작성하였습니다.
+INSERT INTO chat_log("chatUUID", "userID", "message")
+  VALUES
+    ((SELECT uuid FROM chat LIMIT 1), 'yshin', 'yshin 채팅방1 메시지'),
+    ((SELECT uuid FROM chat LIMIT 1), 'holee', 'holee 채팅방1 메시지'),
+    ((SELECT uuid FROM chat LIMIT 1 OFFSET 3), 'yshin', 'yshin 채팅방3 메시지'),
+    ((SELECT uuid FROM chat LIMIT 1 OFFSET 3), 'jwon', 'jwon 채팅방3 메시지'),
+    ((SELECT uuid FROM chat LIMIT 1 OFFSET 3), 'yechoi', 'yechoi 채팅방3 메시지')
+;
