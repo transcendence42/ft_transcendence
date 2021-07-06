b8040ce83a3df52605ae79888cdcfb541423be49
Hochan Lee <holee@c7r7s8.42seoul.kr>
2021-07-06 12:17:09 +0900

    feat: add sql cheatsheet

---
+++ back/cheatsheet.sql
@@ -0,0 +1,7 @@
+SELECT * FROM alarm;
+
+INSERT INTO alarm ("userID", title, content, checked, "type", link) VALUES
+('holee', '친구요청', '42_Dall님이 친구 요청을 보냈습니다.', false, 'addFriend', '/profile'),
+('holee', '게임패배', 'jwon님과 경기에서 패배했습니다.', false, 'defeat', '/profile'),
+('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile'),
+('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile');
