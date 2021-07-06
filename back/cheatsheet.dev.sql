5607eb4189ec11bf8733f290ec07c1b35172db96
yechoi42 <yechoi@student.42seoul.kr>
2021-07-06 12:54:08 +0900

    feat: add game data

--- back/cheatsheet.dev.sql
+++ back/cheatsheet.dev.sql
@@ -14,3 +14,9 @@ INSERT INTO alarm ("userID", title, content, checked, "type", link) VALUES
 ('holee', '게임패배', 'jwon님과 경기에서 패배했습니다.', false, 'defeat', '/profile'),
 ('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile'),
 ('holee', '게임승리', 'Polarbear 님과 경기에서 승리했습니다.', false, 'win', '/profile');
+
+INSERT INTO game ("playerOneID", "playerOneScore", "playerTwoID", "playerTwoScore") VALUES
+('holee', 0, 'yshin', 1),
+('yechoi', 3, 'yshin', 7),
+('holee', 2, 'jwon', 5),
+('yshin', 6, 'jwon', 4);
