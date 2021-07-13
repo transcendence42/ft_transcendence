5ce30a8cfc026607ea1721ab2af952c8d5ba5a36
hochan222 <hochan049@gmail.com>
2021-07-13 22:00:48 +0900

    refactor: 안쓰는 cookie 라이브러리 삭제

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ChakraProvider, Flex, Box, theme, Center } from '@chakra-ui/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { CookiesProvider, withCookies } from 'react-cookie';
 import { Navigation } from './UI/organisms/Navigation';
 import { Alarm } from './UI/organisms/Alarm';
 import Main from './pages/Main';
@@ -9,49 +8,34 @@ import Profile from './pages/Profile';
 import Chat from './pages/Chat';
 import Game from './pages/Game';
 import Login from './pages/Login';
-import Cookies from 'js-cookie';
 
 const App = () => {
-  const [hasCookie, setHasCookie] = useState(false);
-  const cookies = Cookies.get('access_token');
-  console.log('docu cookie');
-  console.log(document.cookie);
-
-  useEffect(() => {
-    if (cookies && cookies !== 'undefined') {
-      setHasCookie(true);
-      console.log(cookies);
-    }
-  }, [cookies]);
-
   return (
     <ChakraProvider theme={theme}>
       <Router>
-        <CookiesProvider>
-          {hasCookie ? (
-            <Flex minH="100vh" minWidth="1395px" flexDirection="row">
-              <Box width="85px" minWidth="85px" maxWidth="85px">
-                <Navigation />
-              </Box>
-              <Center width="full" minWidth="965px">
-                <Switch>
-                  <Route exact path="/" component={Main} />
-                  <Route path="/profile" component={Profile} />
-                  <Route path="/chat" component={Chat} />
-                  <Route path="/game" component={Game} />
-                </Switch>
-              </Center>
-              <Box width="390px" minWidth="390px" maxWidth="390px">
-                <Alarm />
-              </Box>
-            </Flex>
-          ) : (
-            <Login />
-          )}
-        </CookiesProvider>
+        {document.cookie ? (
+          <Flex minH="100vh" minWidth="1395px" flexDirection="row">
+            <Box width="85px" minWidth="85px" maxWidth="85px">
+              <Navigation />
+            </Box>
+            <Center width="full" minWidth="965px">
+              <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/chat" component={Chat} />
+                <Route path="/game" component={Game} />
+              </Switch>
+            </Center>
+            <Box width="390px" minWidth="390px" maxWidth="390px">
+              <Alarm />
+            </Box>
+          </Flex>
+        ) : (
+          <Login />
+        )}
       </Router>
     </ChakraProvider>
   );
 };
 
-export default withCookies(App);
+export default App;
