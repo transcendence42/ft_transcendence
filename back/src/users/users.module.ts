6c25cd20efc193e8aff680b7a966055dcb54b885
yechoi42 <yechoi@student.42seoul.kr>
2021-07-20 21:55:52 +0900

    feat: nestjs-admin complete

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -4,6 +4,9 @@ import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { DefaultAdminModule, DefaultAdminSite } from 'nestjs-admin';
+import { Alarm } from 'src/alarms/entities/alarm.entity';
+import { Chat } from 'src/chats/entities/chat.entity';
+import { ChatLog } from 'src/chat-logs/entities/chat-log.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), DefaultAdminModule],
@@ -14,5 +17,8 @@ export class UsersModule {
   constructor(private readonly adminSite: DefaultAdminSite) {
     // Register the User entity under the "User" section
     adminSite.register('User', User);
+    adminSite.register('Alarm', Alarm);
+    adminSite.register('Chat', Chat);
+    adminSite.register('ChatLog', ChatLog);
   }
 }
