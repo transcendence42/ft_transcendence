a068769f69dca4733dfe1febd59983044d24b456
yechoi42 <yechoi@student.42seoul.kr>
2021-07-22 20:16:27 +0900

    refactor: nestjs env file name

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -30,7 +30,7 @@ adminBro.registerAdapter({ Database, Resource });
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.development' : '.env.production',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
         DB_HOST: Joi.string().required(),
