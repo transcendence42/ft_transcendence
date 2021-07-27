74ff3b7ab032bdf8f304e0877acb34a622c85f2b
hochan Lee <hochan049@gmail.com>
2021-07-27 13:20:04 +0900

    feat: alarm chat people (#135)
    * feat: alarm cheat people
    
    * feat: no contextmenu self
    
    * Update front/src/UI/molecules/Alarm/AlarmChatPeople/index.tsx

--- front/src/UI/molecules/Alarm/index.tsx
+++ front/src/UI/molecules/Alarm/index.tsx
@@ -1,5 +1,6 @@
 import { AlarmMessage } from './AlarmMessage';
 import { AlarmUser } from './AlarmUser';
 import { AlarmChatMessage } from './AlarmChatMessage';
+import { AlarmChatPeople } from './AlarmChatPeople';
 
-export { AlarmMessage, AlarmUser, AlarmChatMessage };
+export { AlarmMessage, AlarmUser, AlarmChatMessage, AlarmChatPeople };
