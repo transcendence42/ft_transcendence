cf2c8aaacdaea80d412b79937426a9c578ba71b5
yechoi42 <yechoi@student.42seoul.kr>
2021-07-30 14:43:20 +0900

    feat: profilegamerecords empty file

---
+++ front/src/UI/molecules/ProfileGameRecords/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ProfileGameRecords = () => {
+  return <div>hello</div>;
+};
--- front/src/pages/Profile/index.tsx
+++ front/src/pages/Profile/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MiddleSectionTemplate from '../../templates/MiddleSection';
+import { ProfileGameRecords } from '../../UI/molecules/ProfileGameRecords';
 import ProfilePageHeader from '../../UI/organisms/ProfilePageHeader';
 
 const Profile: React.FC = (props) => {
   return (
-    <MiddleSectionTemplate
-      middleSectionHeader={<ProfilePageHeader userID={props.match.params.userID} />}
-    ></MiddleSectionTemplate>
+    <MiddleSectionTemplate middleSectionHeader={<ProfilePageHeader userID={props.match.params.userID} />}>
+      <ProfileGameRecords />
+    </MiddleSectionTemplate>
   );
 };
 
