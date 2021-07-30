c8f76d4ce4c120affce2fc43ce003afe4b41d3d8
jwon42 <jwon@student.42seoul.kr>
2021-07-30 16:42:08 +0900

    fix: currentuser (in game)

--- back/src/games/games.resolver.ts
+++ back/src/games/games.resolver.ts
@@ -1,12 +1,15 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, GqlExecutionContext } from '@nestjs/graphql';
 import { GamesService } from './games.service';
 import { Game } from './entities/game.entity';
 import { CreateGameInput } from './dto/create-game.input';
 import { UpdateGameInput } from './dto/update-game.input';
 import { User } from 'src/users/entities/user.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/gql.guard';
 import { CurrentUser } from 'src/users/users.resolver';
 
 @Resolver(() => Game)
+@UseGuards(GqlAuthGuard)
 export class GamesResolver {
   constructor(private readonly gamesService: GamesService) { }
 
@@ -27,7 +30,7 @@ export class GamesResolver {
 
   @Query(() => [Game], { name: 'myGameRecords' })
   findMyGameRecords(@CurrentUser() user: User) {
-    // console.log(user.userID);
+    console.log(user);
     return this.gamesService.findByUserID(user.userID);
   }
 
