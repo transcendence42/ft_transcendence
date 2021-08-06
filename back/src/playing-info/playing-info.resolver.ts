ad38d1c5fa0e7dcc0e69ed028317215203a06de8
hochan222 <hochan049@gmail.com>
2021-08-06 17:46:59 +0900

    fix: sync

--- back/src/playing-info/playing-info.resolver.ts
+++ back/src/playing-info/playing-info.resolver.ts
@@ -5,38 +5,6 @@ import { CreatePlayingInfoInput } from './dto/create-playing-info.input';
 import { UpdatePlayingInfoInput } from './dto/update-playing-info.input';
 import { PubSubProvider } from '../pub-sub/pub-sub.provider';
 
-const data = {
-  canvas: {
-    height: 800,
-    width: 900,
-  },
-  ball: {
-    x: 450,
-    y: 400,
-    velocityX: 5,
-    velocityY: 5,
-    radius: 10,
-    speed: 30,
-    color: 'white',
-  },
-  player1: {
-    x: 0,
-    y: 200,
-    paddleWidth: 10,
-    paddleHeight: 150,
-    color: 'white',
-    score: 0,
-  },
-  player2: {
-    x: 890,
-    y: 200,
-    paddleWidth: 10,
-    paddleHeight: 150,
-    color: 'white',
-    score: 0,
-  },
-};
-
 @Resolver(() => PlayingInfo)
 export class PlayingInfoResolver {
   constructor(
@@ -68,10 +36,6 @@ export class PlayingInfoResolver {
   async updatePlayingInfo(@Args('playingInfoInput') updatePlayingInfoInput: UpdatePlayingInfoInput) {
     const playingInfo = await this.playingInfoService.update(updatePlayingInfoInput.index, {
       ...updatePlayingInfoInput,
-      ballX: updatePlayingInfoInput.ballX + updatePlayingInfoInput.ballVelocityX,
-      ballY: updatePlayingInfoInput.ballY + updatePlayingInfoInput.ballVelocityY,
-      ballVelocityX: updatePlayingInfoInput.ballVelocityX,
-      ballVelocityY: updatePlayingInfoInput.ballVelocityY,
     });
     this.pubSubProvider.getPubSub().publish('playingInfo', {
       playingInfo: {
@@ -81,54 +45,9 @@ export class PlayingInfoResolver {
     return playingInfo;
   }
 
-  collision(playingInfo) {
-    const { ballX, ballY, player1Y, player2Y, ballVelocityX, ballVelocityY } = playingInfo;
-
-    if (ballY - data.ball.radius < 0 || ballY + data.ball.radius > data.canvas.height) {
-      return { ...playingInfo, ballVelocityY: (playingInfo.ballVelocityY *= -1) };
-    }
-
-    // player1 paddle collision
-    if (ballX - data.ball.radius <= 0) {
-      if (player1Y + data.player1.paddleHeight >= ballY && player1Y <= ballY) {
-        return { ...playingInfo, ballVelocityX: (playingInfo.ballVelocityX *= -1) };
-      } else {
-        return {
-          ...playingInfo,
-          player2Score: (playingInfo.player2Score += 1),
-          ballX: data.canvas.width / 2 - ballVelocityX,
-          ballY: data.canvas.height / 2 - ballVelocityY,
-          // gameStatus: 'end',
-        };
-      }
-    }
-
-    // player2 paddle collision
-    if (ballX + data.ball.radius >= data.canvas.width) {
-      if (player2Y + data.player2.paddleHeight >= ballY && player2Y <= ballY) {
-        return { ...playingInfo, ballVelocityX: (playingInfo.ballVelocityX *= -1) };
-      } else {
-        return {
-          ...playingInfo,
-          player1Score: (playingInfo.player1Score += 1),
-          ballX: data.canvas.width / 2 - ballVelocityX,
-          ballY: data.canvas.height / 2 - ballVelocityY,
-          // gameStatus: 'end',
-        };
-      }
-    }
-    return playingInfo;
-  }
-
   async updateBall() {
-    const playingInfo = this.collision(await this.playingInfoService.findOne(1));
-    const updatePlayingInfo = await this.playingInfoService.update(1, {
-      ...playingInfo,
-      ballX: playingInfo.ballX + playingInfo.ballVelocityX,
-      ballY: playingInfo.ballY + playingInfo.ballVelocityY,
-      ballVelocityX: playingInfo.ballVelocityX,
-      ballVelocityY: playingInfo.ballVelocityY,
-    });
+    // const playingInfo = this.collision(await this.playingInfoService.findOne(1));
+    const updatePlayingInfo = await this.playingInfoService.update(1, null);
     this.pubSubProvider.getPubSub().publish('playingInfo', {
       playingInfo: updatePlayingInfo,
     });
--- back/src/playing-info/playing-info.service.ts
+++ back/src/playing-info/playing-info.service.ts
@@ -4,6 +4,37 @@ import { CreatePlayingInfoInput } from './dto/create-playing-info.input';
 import { UpdatePlayingInfoInput } from './dto/update-playing-info.input';
 import { PlayingInfo } from './entities/playing-info.entity';
 
+const data = {
+  canvas: {
+    height: 800,
+    width: 900,
+  },
+  ball: {
+    x: 450,
+    y: 400,
+    velocityX: 5,
+    velocityY: 5,
+    radius: 10,
+    speed: 30,
+    color: 'white',
+  },
+  player1: {
+    x: 0,
+    y: 200,
+    paddleWidth: 10,
+    paddleHeight: 150,
+    color: 'white',
+    score: 0,
+  },
+  player2: {
+    x: 890,
+    y: 200,
+    paddleWidth: 10,
+    paddleHeight: 150,
+    color: 'white',
+    score: 0,
+  },
+};
 @Injectable()
 export class PlayingInfoService {
   async create(createPlayingInfoInput: CreatePlayingInfoInput) {
@@ -34,43 +65,66 @@ export class PlayingInfoService {
     return playingInfo;
   }
 
+  collision(playingInfo) {
+    const { ballX, ballY, player1Y, player2Y, ballVelocityX, ballVelocityY } = playingInfo;
+
+    if (ballY - data.ball.radius < 0 || ballY + data.ball.radius > data.canvas.height) {
+      return { ...playingInfo, ballVelocityY: (playingInfo.ballVelocityY *= -1) };
+    }
+
+    // player1 paddle collision
+    if (ballX - data.ball.radius <= 0) {
+      if (player1Y + data.player1.paddleHeight >= ballY && player1Y <= ballY) {
+        return { ...playingInfo, ballVelocityX: (playingInfo.ballVelocityX *= -1) };
+      } else {
+        return {
+          ...playingInfo,
+          player2Score: (playingInfo.player2Score += 1),
+          ballX: data.canvas.width / 2 - ballVelocityX,
+          ballY: data.canvas.height / 2 - ballVelocityY,
+          // gameStatus: 'end',
+        };
+      }
+    }
+
+    // player2 paddle collision
+    if (ballX + data.ball.radius >= data.canvas.width) {
+      if (player2Y + data.player2.paddleHeight >= ballY && player2Y <= ballY) {
+        return { ...playingInfo, ballVelocityX: (playingInfo.ballVelocityX *= -1) };
+      } else {
+        return {
+          ...playingInfo,
+          player1Score: (playingInfo.player1Score += 1),
+          ballX: data.canvas.width / 2 - ballVelocityX,
+          ballY: data.canvas.height / 2 - ballVelocityY,
+          // gameStatus: 'end',
+        };
+      }
+    }
+    return playingInfo;
+  }
+
   async update(index: number, updatePlayingInfoInput: UpdatePlayingInfoInput) {
     const playingInfo = await PlayingInfo.findOne(index);
-    if (updatePlayingInfoInput.uuid) {
-      playingInfo.uuid = updatePlayingInfoInput.uuid;
-    }
-    if (updatePlayingInfoInput.ballX || updatePlayingInfoInput.ballX === 0) {
-      playingInfo.ballX = updatePlayingInfoInput.ballX;
-    }
-    if (updatePlayingInfoInput.ballY || updatePlayingInfoInput.ballY === 0) {
-      playingInfo.ballY = updatePlayingInfoInput.ballY;
-    }
-    if (updatePlayingInfoInput.ballVelocityX || updatePlayingInfoInput.ballVelocityX === 0) {
-      playingInfo.ballVelocityX = updatePlayingInfoInput.ballVelocityX;
-    }
-    if (updatePlayingInfoInput.ballVelocityY || updatePlayingInfoInput.ballVelocityY === 0) {
-      playingInfo.ballVelocityY = updatePlayingInfoInput.ballVelocityY;
-    }
-    if (updatePlayingInfoInput.player1Y || updatePlayingInfoInput.player1Y === 0) {
-      playingInfo.player1Y = updatePlayingInfoInput.player1Y;
-    }
-    if (updatePlayingInfoInput.player2Y || updatePlayingInfoInput.player2Y === 0) {
-      playingInfo.player2Y = updatePlayingInfoInput.player2Y;
-    }
-    if (updatePlayingInfoInput.player1Score || updatePlayingInfoInput.player1Score === 0) {
-      playingInfo.player1Score = updatePlayingInfoInput.player1Score;
-    }
-    if (updatePlayingInfoInput.player2Score || updatePlayingInfoInput.player2Score === 0) {
-      playingInfo.player2Score = updatePlayingInfoInput.player2Score;
-    }
-    playingInfo.sequence = playingInfo.sequence + 1;
-    const validate_error = await validate(playingInfo);
+
+    playingInfo.ballX = playingInfo.ballX + playingInfo.ballVelocityX;
+    playingInfo.ballY = playingInfo.ballY + playingInfo.ballVelocityY;
+
+    const collisionInfoInput = this.collision({
+      ...playingInfo,
+      ...updatePlayingInfoInput,
+      ballX: playingInfo.ballX,
+      ballY: playingInfo.ballY,
+      sequence: playingInfo.sequence + 1,
+    });
+
+    const validate_error = await validate(collisionInfoInput);
     if (validate_error.length > 0) {
       const _error = { playingInfo: 'PlayingInfo Input is not valid' };
       throw new HttpException({ message: 'PlayingInfo Input validation failed', _error }, HttpStatus.BAD_REQUEST);
     } else {
-      await PlayingInfo.save(playingInfo);
-      return playingInfo;
+      await PlayingInfo.save(collisionInfoInput);
+      return collisionInfoInput;
     }
   }
 
