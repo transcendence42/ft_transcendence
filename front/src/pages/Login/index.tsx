dbb8da4adf19b300785716bceac1ab6c6ba03387
Junho Won <61400214+jwon42@users.noreply.github.com>
2021-10-19 21:59:11 +0900

    feat: logo (login page, navigation bar) (#145)
    * feat: add Particle BG atom
    
    * feat: delete images folder
    
    * refactor: login page
    
    * fix: yarn.lock
    
    * feat: to pull
    
    * feat: add logo_navabar.png
    
    * feat: apply navbar logo
    
    * feat: delete schema

--- front/src/pages/Login/index.tsx
+++ front/src/pages/Login/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
+import { Button, Center } from '@chakra-ui/react';
+import { ParticleBackground } from '../../UI/atoms/ParticleBackground';
 
 const login = () => {
   window.location.href = `${process.env.REACT_APP_SERVER_URL}/auth/login`;
 };
 
 const Login: React.FC = () => {
-  return <Button onClick={login}>42 Login</Button>;
+  return (
+    <>
+      <ParticleBackground />
+      <Center height="100vh">
+        <Button onClick={login}>42 Login</Button>
+      </Center>
+    </>
+  );
 };
 
 export default Login;
--- front/src/pages/TwoFactorAuth/index.tsx
+++ front/src/pages/TwoFactorAuth/index.tsx
@@ -1,11 +1,15 @@
 import { Center } from '@chakra-ui/react';
 import React from 'react';
+import { ParticleBackground } from '../../UI/atoms/ParticleBackground';
 import { TwoFactorAuthBox } from '../../UI/organisms/TwoFactorAuthBox';
 
 export const TwoFactorAuth: React.FC = () => {
   return (
-    <Center>
-      <TwoFactorAuthBox />
-    </Center>
+    <>
+      <ParticleBackground />
+      <Center>
+        <TwoFactorAuthBox />
+      </Center>
+    </>
   );
 };
