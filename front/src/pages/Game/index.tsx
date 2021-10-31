66fcbe68d45dcbeaeace46a96a57b1f3ac4bc25a
hochan Lee <hochan049@gmail.com>
2021-10-31 17:33:23 +0900

    fix: docker postgres version err (#167)
    * fix: docker postgres version err
    
    * fix: change history.push to window~
    
    Co-authored-by: yechoi42 <yechoi@student.42seoul.kr>
    Co-authored-by: jwon42 <jwon@student.42seoul.kr>
    Co-authored-by: yhshin0 <asyh1047@naver.com>

--- front/src/pages/Game/index.tsx
+++ front/src/pages/Game/index.tsx
@@ -4,13 +4,13 @@ import GamePageHeader from '../../UI/organisms/GamePageHeader';
 import { useQuery } from '@apollo/client';
 import { GET_MY_GAME_RECORDS } from './GameQuery';
 import { CrazyPongContainer } from '../../UI/organisms/CrazyPong';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 const Game: React.FC = () => {
   const { loading, error, data } = useQuery(GET_MY_GAME_RECORDS, {
     pollInterval: 1000,
   });
-  const history = useHistory();
+  // const history = useHistory();
 
   if (loading) {
     return <p>loading</p>;
@@ -20,8 +20,8 @@ const Game: React.FC = () => {
     return <p>game component error</p>;
   }
   if (!data.myGameRecords[data.myGameRecords.length - 1]) {
-    history.push('/');
-    // window.location.replace(String(process.env.REACT_APP_CLIENT_URL));
+    // history.push('/');
+    window.location.replace(String(process.env.REACT_APP_CLIENT_URL));
     return <>game over</>;
   } else {
     return (
